490e4f99e09c19bddccb96683550139ec87bbb8e
Niv <nivdan7@gmial.com>
2021-12-25 01:15:49 +0200

    Added colors to strings , TODO: check in labs

--- Client.py
+++ Client.py
@@ -3,19 +3,29 @@ import sys
 from select import select
 from socket import *
 
+# Colors for strings
+HEADER = '\033[95m'
+OKBLUE = '\033[94m'
+OKGREEN = '\033[92m'
+YELLOW = '\033[93m'
+RED = '\033[91m'
+ENDC = '\033[0m'
+BOLD = '\033[1m'
+
+
 # Magic numbers
 TIMEOUT = 10
 UDP_serverPort = 13117
 BUFFER_SIZE = 1024
 EXPECTED_COOKIE = 0xabcddcba
 EXPECTED_OFFER_TYPE = 0x2
-TEAM_NAME = 'HAFALAFEL HA-YA-ROCK!\n'
-INVALID_OFFER_FORMAT = "Invalid expected offer format"
+TEAM_NAME = 'Regina Phalange\n'
+INVALID_OFFER_FORMAT = YELLOW+"Invalid expected offer format"+ENDC
 
 
 def run():
     # Open UDP socket to listen for broadcast from servers
-    print("Client started, listening for offer requests...")
+    print(HEADER+"Client started, listening for offer requests...")
     UDP_clientSocket = socket(AF_INET, SOCK_DGRAM)
     UDP_clientSocket.setsockopt(SOL_SOCKET,SO_REUSEADDR, 1)
     UDP_clientSocket.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
@@ -24,7 +34,7 @@ def run():
     UDP_clientSocket.bind(('', UDP_serverPort))  # (IP,PORT) - listen to specific port
     while True:
         offer, serverAddress = UDP_clientSocket.recvfrom(BUFFER_SIZE)  # blocking
-        print("Received offer from {0}, attempting to connect...".format(serverAddress[0]))
+        print(OKGREEN+BOLD+"Received offer from {0}, attempting to connect...".format(serverAddress[0]))
         TCP_clientSocket = socket(AF_INET, SOCK_STREAM)
         try:
             # check that the offer is valid as instructed
@@ -40,7 +50,7 @@ def run():
                 print(msg)
                 # open timer for 10 seconds so that we can answer
                 answerQuestion(TCP_clientSocket)
-                print("Server disconnected, listening for offer requests...")
+                print(OKBLUE + "Server disconnected, listening for offer requests...")
                 TCP_clientSocket.close()
             else:
                 print(INVALID_OFFER_FORMAT)
@@ -52,16 +62,14 @@ def run():
 def answerQuestion(TCP_clientSocket):
     try:
         reads, _, _ = select([sys.stdin, TCP_clientSocket], [], [], TIMEOUT)
-        if len(reads) > 0:
-            if reads[0] == sys.stdin:
-                ans = sys.stdin.readline().encode()
-                TCP_clientSocket.send(ans)
-            elif reads[0] == TCP_clientSocket:
-                ans = TCP_clientSocket.recv(BUFFER_SIZE).decode()
-                print(ans)
+        if len(reads) > 0 and reads[0] == sys.stdin:
+            ans = sys.stdin.readline().encode()
+            TCP_clientSocket.send(ans)
+        ans_server = TCP_clientSocket.recv(BUFFER_SIZE).decode()
+        print(ans_server)
     except:
         TCP_clientSocket.close()
-        print("Something went wrong in the answerQuestion method")
+        print(RED+"Something went wrong in the answerQuestion method")
 
 
 
--- Server.py
+++ Server.py
@@ -1,5 +1,4 @@
 import struct
-import sys
 import time
 from random import random, randint
 from select import select
@@ -7,6 +6,21 @@ from scapy.arch import get_if_addr
 from socket import *
 from threading import Thread
 
+# Colors for strings
+HEADER = '\033[95m'
+OKBLUE = '\033[94m'
+OKCYAN = '\033[96m'
+OKGREEN = '\033[92m'
+YELLOW = '\033[93m'
+RED = '\033[91m'
+ENDC = '\033[0m'
+BOLD = '\033[1m'
+UNDERLINE = '\033[4m'
+GAME_OVER = "{0}G{1}A{2}M{3}E{4} {5}O{6}V{7}E{8}R{9}". \
+    format(RED, OKGREEN, OKBLUE, YELLOW, ENDC, RED, OKGREEN, OKBLUE, YELLOW, ENDC)
+CONNECTION_LOST = YELLOW + "Connection lost from clients during the game" + ENDC
+
+# MAGIC NUMBERS
 TIMEOUT = 10
 BUFFER_SIZE = 1024
 TYPE = 0x2
@@ -40,33 +54,32 @@ def openTCPSocket():
     # serverSocket.bind((serverAddress, TCP_serverPORT))
     serverSocket.bind(('', TCP_serverPORT))
     serverSocket.listen(2)
-    print("Server started, listening on IP {0}".format(serverAddress))
+    print(UNDERLINE + "Server started, listening on IP {0}".format(serverAddress))
     return serverSocket
 
 
 def generateQuestion():
     n1 = randint(20, 100)
     n2 = randint(n1 - 9, n1)
-    return "{0}-{1}".format(n1, n2), (n1 - n2)
+    return YELLOW + "{0}-{1}".format(n1, n2), (n1 - n2)
 
 
 def generateStartMsg(team1_name, team2_name):
     question, ans = generateQuestion()
-    return "Welcome to Quick Math.\n" \
-           "Player 1: {0}Player 2: {1}==\n" \
-           "Please answer the following question as fast as you can:\n" \
-           "How much is {2}?\n".format(
+    players = YELLOW + "Player 1: " + team1_name + "Player 2: " + team2_name + BOLD + ENDC + "==" + "\n"
+    return OKCYAN + "Welcome to Quick Math.\n" + ENDC + players + UNDERLINE + \
+           "Please answer the following question as fast as you can:\n" + ENDC + RED + "How much is {2}?\n".format(
         team1_name, team2_name, question), str(ans)
 
 
 def generateEndMsg(team1_name, team1_ans, team2_name, team2_ans, ans):
-    winner = "No one answered correctly\n"
+    winner = HEADER + BOLD + "No team submitted an answer...\n"
     if (team1_ans[0] and team1_ans[1] == ans) or (team2_ans[0] and team2_ans[1] != ans):
-        winner = "Congratulations to the winner: {0}\n".format(team1_name)
+        winner = OKGREEN + "Congratulations to the winner: {0}\n".format(team1_name)
     elif (team2_ans[0] and team2_ans[1] == ans) or (team1_ans[0] and team1_ans[1] != ans):
-        winner = "Congratulations to the winner: {0}\n".format(team2_name)
-    msg = "Game Over!\n" \
-          "The correct answer was {0}!\n".format(ans) + winner
+        winner = OKGREEN + "Congratulations to the winner: {0}\n".format(team2_name)
+    msg = GAME_OVER + "\n" + \
+          OKBLUE + BOLD + "The correct answer was {0}!\n".format(YELLOW + ans + ENDC) + winner
     return msg
 
 
@@ -88,9 +101,9 @@ def initGame():
         reads, _, _ = select([team1_socket, team2_socket], [], [], TIMEOUT)
         if len(reads) > 0:
             if reads[0] == team1_socket:
-                team1_ans = (True, team1_socket.recv(BUFFER_SIZE).decode())
+                team1_ans = (True, team1_socket.recv(BUFFER_SIZE).decode()[:-1])  # Remove the '\n' char from the end
             elif reads[0] == team2_socket:
-                team2_ans = (True, team2_socket.recv(BUFFER_SIZE).decode())
+                team2_ans = (True, team2_socket.recv(BUFFER_SIZE).decode()[:-1])  # Remove the '\n' char from the end
         end_msg = generateEndMsg(team1_name, team1_ans, team2_name, team2_ans, ans)
         # send back results
         team1_socket.send(end_msg.encode())
@@ -99,9 +112,8 @@ def initGame():
         team2_socket.close()
         connectedPlayers = []  # clean list and ready for next session
     except:
-        team1_socket.close()
-        team2_socket.close()
-        print("Something went wrong during the game")
+        print(CONNECTION_LOST)
+        connectedPlayers = []  # clean list and ready for next session
 
 
 def run():
@@ -114,7 +126,7 @@ def run():
         sendBroadcast_thread.join()
         acceptClients_thread.join()
         initGame()
-        print("Game over, sending out offer requests...")
+        print("{0}, {1}sending out offer requests...{2}".format(GAME_OVER, HEADER, ENDC + BOLD))
 
 
 if __name__ == "__main__":
